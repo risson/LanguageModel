27b6c7405845a2a202486b49528bda29c9321d7e
Risson Yao <Risson.Yao@ef.com>
2018-07-13 16:27:36 +0800

    fixed bugs

--- Char_LSTM_LM.py
+++ Char_LSTM_LM.py
@@ -1,5 +1,5 @@
 import numpy as np
-import keras as K
+from keras import backend as K
 from keras.layers import LSTM, Dense, Activation
 from keras import Sequential
 from keras.preprocessing.text import Tokenizer
@@ -19,39 +19,40 @@ class LMCharLSTM():
         tokenizer = Tokenizer(char_level=True)
         tokenizer.fit_on_texts([texts])
         self.charindex = tokenizer.word_index
-        self.indexchar = dict(zip(_[1],_[0]) for _ in self.charindex.items())
+        self.indexchar = {value:key for key, value in self.charindex.items()}
         self.diversity = [0.2, 0.4, 0.6, 0.8]
 
-        self._model = self.__init_model__()
+        self._model = self.__init_model()
 
     
     def __init_model(self):
         model = Sequential()
         model.add(LSTM(512, activation='tanh', return_sequences=True))
         model.add(LSTM(256, activation='tanh', return_sequences=False))
-        model.add(Dense(len(self.chardict)))
+        model.add(Dense(len(self.charindex)))
         model.add(Activation('softmax'))
         model.compile(optimizer='adam',loss='categorical_crossentropy')
         return model 
 
     def train(self, epochs=50, batch_size=256):
 
+        texts = utils.generate_big_text(self._txtfile)
+
         def epoch_end_print(epoch, logs):
             print()
             print('-----Generate text for epoch {}: -----'.format(epoch))
-            texts = utils.generate_big_text(txtfile)
-
-            i = np.random.randint(len(texts)-self.maxlen)
+            i = np.random.randint(len(texts)-self.maxlen-1)
             sentence = texts[i:i+self.maxlen]
             print('Seed text: ')
             print('"' + sentence + '"')
             print('Generated text: ')
-            
-            for _ in range(300):
+            sys.stdout.write(sentence)
+
+            for _ in range(30):
                 
                 reader = utils.sentence_reader(sentence, self.maxlen, self.charindex, 'test')
-                vec = next(reader)
-                preds = self._model.predict(np.array(vec), verbose=0)[0]
+                vec = next(reader())
+                preds = self._model.predict(np.array([vec]), verbose=0)[0]
                 nextcharindex = np.random.choice(len(self.charindex), p=preds)
                 nextchar = self.indexchar[nextcharindex]
                 sentence = sentence[1:]+nextchar
@@ -60,51 +61,54 @@ class LMCharLSTM():
                 sys.stdout.flush()
 
             print()
-                
+        
+        reader = utils.sentence_reader(texts, self.maxlen, self.charindex, 'train')
+
         #----- Read one sentence at a time
-        for epoch in range(epochs):
-            texts = utils.generate_big_text(self._txtfile)
-            del texts
-            X_batch = []
-            y_batch = []
-            loss_list = []
-            batch_id = 0
+        # for epoch in range(epochs):
+        #     texts = utils.generate_big_text(self._txtfile)
+        #     del texts
+        #     X_batch = []
+        #     y_batch = []
+        #     loss_list = []
+        #     batch_id = 0
 
-            for sentence, nextchar in utils.sentence_reader(texts, self.maxlen, self.charindex, 'train'):    
-                batch_id += 1               
+        #     for sentence, nextchar in reader():    
+        #         batch_id += 1               
                 
-                if batch_id%(batch_size*1000)==0:
-                    print('{} - Epoch {}: Batch_id - {}: Loss = {}'.format(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"),
-                          epoch, batch_id, np.mean(loss_list)))
-                    loss_list = []
+        #         if batch_id%(batch_size*1000)==0:
+        #             print('{} - Epoch {}: Batch_id - {}: Loss = {}'.format(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"),
+        #                   epoch, batch_id, np.mean(loss_list)))
+        #             loss_list = []
 
-                if batch_id%batch_size==0:
+        #         if batch_id%batch_size==0:
                     
-                    loss = self._model.train_on_batch(np.array(X_batch),np.array(y_batch))
-                    loss_list.append(loss)
-                    X_batch = []
-                    y_batch = []
+        #             loss = self._model.train_on_batch(np.array(X_batch),np.array(y_batch))
+        #             loss_list.append(loss)
+        #             X_batch = []
+        #             y_batch = []
                 
-                X_batch.append(sentence)
-                y_batch.append(nextchar)
+        #         X_batch.append(sentence)
+        #         y_batch.append(nextchar)
             
-            epoch_end_print(epoch,None)
+        #     epoch_end_print(epoch,None)
 
         #----- Read all data into memory:
-        reader = utils.sentence_reader(self._txtfile, self.maxlen, self.charindex)
         X = []
         y = []
+        
         for sentence, nextchar in reader():
             X.append(sentence)
             y.append(nextchar)
-
+ 
         print_callback = LambdaCallback(on_epoch_end=epoch_end_print)
-        self._model.fit(X, y,
+        self._model.fit(np.array(X), np.array(y),
                         epochs=50,
                         batch_size=256,
                         callbacks=[print_callback])
 
 
 if __name__ == '__main__':
-    text_gen = LMCharLSTM()
+    txtfile = 'reviews_Electronics_5.txt'
+    text_gen = LMCharLSTM(txtfile)
     text_gen.train()
--- utils.py
+++ utils.py
@@ -18,22 +18,24 @@ def generate_big_text(txtfile):
 
 def sentence_reader(texts, maxlen, charindex, mode='train'):
     
-    for i in np.arange(0, len(texts), maxlen):
-        if (i+maxlen>=len(texts)) and mode=='train':
-            break
-        text = texts[i:i+maxlen]
-        sent_X = []
-        for char in text:            
-            
-            char_idx = charindex[char]
-            feat_list = [1 if i==char_idx else 0 for i in range(len(charindex))]
-            sent_X.append(feat_list)
+    def reader():
+        for i in np.arange(0, len(texts), maxlen):
+            if (i+maxlen>=len(texts)) and mode=='train':
+                break
+            text = texts[i:i+maxlen]
+            sent_X = []
+            for char in text:            
+                
+                char_idx = charindex[char]
+                feat_list = [1 if x==char_idx else 0 for x in range(len(charindex))]
+                sent_X.append(feat_list)
 
-        if mode=='test':
-            yield(sent_X)
-        else:
-            sent_y = charindex[texts[i+maxlen]]
-            yield (sent_X, sent_y)
+            if mode=='test':
+                yield(sent_X)
+            else:
+                sent_y = [1 if x==charindex[texts[i+maxlen]] else 0 for x in range(len(charindex))]
+                yield (sent_X, sent_y)
+    return reader
 
 if __name__ == '__main__':
     texts = generate_big_text('C:/Users/risson.yao/word2vec/testpage.txt')
